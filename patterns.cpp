8d8653c720945ca2cd66e9d9d4a22f1ce54a9d9e
Piyush Aggarwal <Piyush.tech2024@gmail.com>
2026-06-01 11:00:00 +0530

    fw

--- patterns.cpp
+++ patterns.cpp
@@ -27,7 +27,27 @@ void print3 (int n ){
      }
 }
 void print4(int n ){
-    
+     for ( int i = 0 ;i<n; i++ ){
+        for (int j = 0 ; j<=i; j++){
+            cout<<i;
+
+        }
+    cout<<endl;
+     }
+
+}
+void print5(int n ){
+for ( int i = 1; i < n; i++){
+    for (int j =0; j<n-i+1;j++){
+        cout<<"*";
+
+    }
+    cout<<endl;
+
+}
+}
+void print6( int n ){
+    for 
 }
 int main(){
     int t;
@@ -35,7 +55,7 @@ int main(){
     for ( int i = 0; i<t; i++){
         int n ;
         cin>>n;
-        print3(n);
+        print5(n);
 
     }
 }
