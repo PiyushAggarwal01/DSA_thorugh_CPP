b9cb7deca58f9b3c85171001ea725963e0ea1ddb
Piyush Aggarwal <Piyush.tech2024@gmail.com>
2026-06-02 15:54:11 +0530

    jvu

--- patterns.cpp
+++ patterns.cpp
@@ -62,9 +62,20 @@ void print6( int n ){
         }
         cout<<endl;
     }
-
-        
-    
+}
+void print7( int n){
+    for ( int i =0; i<n; i++){
+        for ( int j = 0; j < i ; j ++){
+            cout << " ";
+        }
+        for ( int j = 0 ; j < 2*n- 2*i -1; j ++){
+            cout << "*";
+        }
+        for (int j = 0 ; j < i ; j ++){
+            cout <<" ";
+        }
+        cout<<endl;
+    }
 }
 int main(){
     int t;
@@ -72,7 +83,7 @@ int main(){
     for ( int i = 0; i<t; i++){
         int n ;
         cin>>n;
-        print6(n);
+        print7(n);
 
     }
 }
