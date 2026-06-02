1346d8a82a5fe35a8b84f891fd1f9d68c043a93d
Piyush Aggarwal <Piyush.tech2024@gmail.com>
2026-06-02 15:56:58 +0530

    cbfg

--- patterns.cpp
+++ patterns.cpp
@@ -77,13 +77,42 @@ void print7( int n){
         cout<<endl;
     }
 }
+void print8( int n ){
+    for (int i = 0 ; i < n ;i++ ){
+        //space 
+        for (  int j = 0 ; j<n - i -1;  j++){
+            cout<<" ";
+        }
+            //star 
+        for ( int j = 0 ; j < 2*i+1 ; j++){
+            cout<<"*";
+        }
+            //space
+        for (  int j = 0 ; j<n - i -1;  j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    
+    }
+    for ( int i =0; i<n; i++){
+        for ( int j = 0; j < i ; j ++){
+            cout << " ";
+        }
+        for ( int j = 0 ; j < 2*n- 2*i -1; j ++){
+            cout << "*";
+        }
+        for (int j = 0 ; j < i ; j ++){
+            cout <<" ";
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int t;
     cin>>t;
     for ( int i = 0; i<t; i++){
         int n ;
         cin>>n;
-        print7(n);
+        print8(n);
 
     }
-}
