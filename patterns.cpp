3dbd4af973864e1ee44a77e0c4c1a876e6efd011
Piyush Aggarwal <Piyush.tech2024@gmail.com>
2026-06-02 16:08:06 +0530

    done

--- patterns.cpp
+++ patterns.cpp
@@ -111,7 +111,10 @@ void print9( int n  ){
     for ( int i = 1 ; i <=2*n-1; i ++ ){
         int stars = i;
         if ( i>n) stars = 2*n-i;
-        for ( int j )
+        for ( int j =0 ; j < stars ; j ++){
+            cout<<"*";
+        }
+        cout<<endl;
     }
 }
 int main(){
