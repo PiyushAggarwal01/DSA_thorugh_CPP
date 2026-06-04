f290c696ece14bcbfad8df1f7095a4fcc8d79256
Piyush Aggarwal <Piyush.tech2024@gmail.com>
2026-06-04 22:33:58 +0530

    dnj

--- patterns.cpp
+++ patterns.cpp
@@ -181,7 +181,7 @@ void print15( int n ){
   
     for ( int i = 0; i < n ; i ++){
         char ch = 'A' + i ;
-        for ( int j = 0 ; j <= i ; j ++){
+        for ( int k = 0 ; k <= i ; k ++){
            cout<<ch;
 
 
